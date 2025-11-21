154424e7014baa03047cb719c5d2aeb13268a6c6
AlejandroPardoS <a.pardos2@uniandes.edu.co>
2025-11-21 09:20:00 -0500

    all

--- src/asistente/asistente.service.ts
+++ src/asistente/asistente.service.ts
@@ -2,20 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AsistenteEntity } from './asistente.entity';
+import { EventoEntity } from 'src/evento/evento.entity';
 
 @Injectable()
 export class AsistenteService {
     constructor(
         @InjectRepository(AsistenteEntity)
-        private readonly asistenteRepository: Repository<AsistenteEntity>
+        private readonly asistenteRepository: Repository<AsistenteEntity>,
+        
+        @InjectRepository(EventoEntity)
+        private readonly eventoRepository: Repository<EventoEntity>
     ) {}
 
-    async registrarAsistente(id: string, asistente: AsistenteEntity): Promise<AsistenteEntity> {
-        const persistedAsistente = await this.asistenteRepository.findOne({where:{id}});
-        if (!persistedAsistente)
-            throw new Error("The asistente with the given id was not found");
+    async registrarAsistente(eventoId: string, asistente: AsistenteEntity): Promise<AsistenteEntity> {
+        const evento = await this.eventoRepository.findOne({where: { id: eventoId }, relations: ['asistentes']});
+        if (!evento)
+            throw new Error("El evento con el id dado no existe");
 
-        return await this.asistenteRepository.save({...persistedAsistente, ...asistente});
+        if (evento.asistentes.length > evento.auditorio.capacidad)
+            throw new Error("Los asistentes del evento superan la capacidad del auditorio");
+        asistente.evento = evento;
+
+        return await this.asistenteRepository.save(asistente)
    }
+
+    async findAsistentesByEvento(eventoId: string): Promise<AsistenteEntity[]> {
+        const evento = await this.eventoRepository.findOne({where: { id: eventoId }, relations: ["asistentes", "ponente", "auditorio"] });
+        if (!evento)
+            throw new Error("El evento con el id dado no existe");
+        return evento.asistentes;
+    }
 
 }
